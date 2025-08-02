7a3da71deac5588c73d534b932414820acbbcb72
yaominghua <minghuayao1981@gmail.com>
2025-08-02 17:50:00 +0800

    create setup folder for quick setting up dev env

---
+++ unitree_lerobot/setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="unitree_lerobot",
+    version="0.1.0",
+    packages=find_packages(),
+    install_requires=[
+        # 添加必要的依赖
+    ],
+    python_requires=">=3.8",
+)
